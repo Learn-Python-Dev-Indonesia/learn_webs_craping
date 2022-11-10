9612a221aa9741aa086e712506beef3ded74b645
UsernameAnda <piccoaldi@gmail.com>
2022-11-10 09:08:21 +0700

    first commit

--- pusat_utbk_tn.py
+++ pusat_utbk_tn.py
@@ -14,9 +14,15 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-       for i in range (1, 75):
+       for i in range (1, 52):
        # for sekolah in response.css('.table > tbody:nth-child(2)'):
             yield {
-                'Nomer' : response.css('.col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child('+ str(i) +') > td:nth-child(1)::text').extract(),
+                'Nomer' : response.css('.col-md-10 > div:nth-child(4) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(1)::text').extract(),
+                'Kode' : response.css('.col-md-10 > div:nth-child(4) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(2)::text').extract(),
+                'Nama' : response.css('.col-md-10 > div:nth-child(4) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(3)::text').extract(),
+                'Alamat' : response.css('.col-md-10 > div:nth-child(4) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(4)::text').extract(),
             }
-# .col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(5)
+# Nomer
+# .col-md-10 > div:nth-child(4) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(1)
+# Kode
+# .col-md-10 > div:nth-child(4) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(2)
