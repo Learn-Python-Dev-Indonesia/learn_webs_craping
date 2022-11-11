2ea582c83b334f1a205a60ff709eff0da68af8fe
UsernameAnda <piccoaldi@gmail.com>
2022-11-12 05:34:12 +0700

    first commit

--- pusat_utbk.py
+++ pusat_utbk.py
@@ -14,16 +14,15 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-       for i in range (1, 75):
-       # for sekolah in response.css('.table > tbody:nth-child(2)'):
-            yield {
-                'Nomer' : response.css('.col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child('+ str(i) +') > td:nth-child(1)::text').extract(),
-                'Kode' : response.css('.col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child('+ str(i) +') > td:nth-child(2)::text').extract(),
-                'Nama' : response.css('.col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child('+ str(i) +') > td:nth-child(3)::text').extract(),
-                'Alamat' : response.css('.col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child('+ str(i) +') > td:nth-child(4)::text').extract(),
-                'Kode Pos' : response.css('.col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child('+ str(i) +') > td:nth-child(5)::text').extract(),
-                'No Telpon' : response.css('.col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child('+ str(i) +') > td:nth-child(6)::text').extract(),
-                'Email' : response.css('.col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child('+ str(i) +') > td:nth-child(7)::text').extract(),
-
-            }
+        for sekolah in response.css('col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1)'):
+            for i in range(1, 75):
+                yield {
+                    'Nomer' : sekolah.css('tr:nth-child('+ str(i) +') > td:nth-child(1)::text').extract(),
+                    'Kode' : sekolah.css('tr:nth-child('+ str(i) +') > td:nth-child(2)::text').extract(),
+                    'Nama' : sekolah.css('tr:nth-child('+ str(i) +') > td:nth-child(3)::text').extract(),
+                    'Alamat' : sekolah.css('tr:nth-child('+ str(i) +') > td:nth-child(4)::text').extract(),
+                    'Kode Pos' : sekolah.css('tr:nth-child('+ str(i) +') > td:nth-child(5)::text').extract(),
+                    'No Telpon' : sekolah.css('tr:nth-child('+ str(i) +') > td:nth-child(6)::text').extract(),
+                    'Email' : sekolah.css('tr:nth-child('+ str(i) +') > td:nth-child(7)::text').extract(),
+                }
 # .col-md-10 > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(5)
